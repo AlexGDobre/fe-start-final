5af0f9f2276911dc935e2ee9ddad18a02c1ff3a5
AlexHDobre <blakseaok@gmail.com>
2022-10-20 14:21:57 +0300

    initial commit

---
+++ stylesheet/baseheader.js
@@ -0,0 +1,109 @@
+// document.getElementById(#jsheader).innerHTML=("
+// <nav>
+
+//   <img src="cont/abilmente-magazine.png" alt="logo">
+
+//   <ul class="navlist">
+//     <li class="dropdownhover"><span>Magazine</span> 
+//       <ul class="dropdown">
+//         <li><a href="#">blabla1</a> </li>
+//         <li><a href="#">blabla2</a> </li>
+//       </ul>
+//     </li>
+//     <li class="dropdownhover"><span>Corsi</span> 
+//       <ul class="dropdown">
+//         <li><a href="#">blabla1</a> </li>
+//         <li><a href="#">blabla2</a> </li>
+//         <li><a href="#">blabla3</a> </li>
+//         <li><a href="#">bla4</a> </li>
+//       </ul>
+//     </li>
+//     <li class="dropdownhover"><span>Fiere</span> 
+//       <ul class="dropdown">
+//         <li><a href="#">blabla1</a> </li>
+//         <li><a href="#">blabla2</a> </li>
+//       </ul>
+//     </li>
+//     <li class="dropdownhover"><span>Negozi</span> 
+//       <ul class="dropdown">
+//         <li><a href="#">blabla1</a> </li>
+//         <li><a href="#">blabla2</a> </li>
+//         <li><a href="#">blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a> </li>
+//       </ul>
+//     </li>
+//   </ul>
+
+//   <div class="navsearch">
+//     <form action="">
+//       <input type="text">
+//     </form>
+//   </div>
+//   <div class="navprofile">
+//     <ul>
+//       <li class="dropdownhover">
+//         <span><div class="navavatar"></div>Annachiara</span>
+//         <ul class="dropdown">
+//           <li><a href="#">korzina</a></li>
+//           <li><a href="#">Istoria pokupki</a></li>
+//         </ul>
+//       </li>
+//     </ul>
+//   </div>
+
+// </nav>")
+
+
+
+document.write(
+<nav>
+
+  <img src="cont/abilmente-magazine.png" alt="logo">
+
+  <ul class="navlist">
+    <li class="dropdownhover"><span>Magazine</span> 
+      <ul class="dropdown">
+        <li><a href="#">blabla1</a> </li>
+        <li><a href="#">blabla2</a> </li>
+      </ul>
+    </li>
+    <li class="dropdownhover"><span>Corsi</span> 
+      <ul class="dropdown">
+        <li><a href="#">blabla1</a> </li>
+        <li><a href="#">blabla2</a> </li>
+        <li><a href="#">blabla3</a> </li>
+        <li><a href="#">bla4</a> </li>
+      </ul>
+    </li>
+    <li class="dropdownhover"><span>Fiere</span> 
+      <ul class="dropdown">
+        <li><a href="#">blabla1</a> </li>
+        <li><a href="#">blabla2</a> </li>
+      </ul>
+    </li>
+    <li class="dropdownhover"><span>Negozi</span> 
+      <ul class="dropdown">
+        <li><a href="#">blabla1</a> </li>
+        <li><a href="#">blabla2</a> </li>
+        <li><a href="#">blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a> </li>
+      </ul>
+    </li>
+  </ul>
+
+  <div class="navsearch">
+    <form action="">
+      <input type="text">
+    </form>
+  </div>
+  <div class="navprofile">
+    <ul>
+      <li class="dropdownhover">
+        <span><div class="navavatar"></div>Annachiara</span>
+        <ul class="dropdown">
+          <li><a href="#">korzina</a></li>
+          <li><a href="#">Istoria pokupki</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+
+</nav>);
